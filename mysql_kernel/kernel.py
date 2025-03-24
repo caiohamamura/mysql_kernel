b350ae1cf9e311fdbf0cdec8c491b2b4450f2241
caiohamamura <caiohamamura@gmail.com>
2025-03-24 17:39:21 -0300

    Removed pd.io.sql.execute calls (deprecated).

--- mysql_kernel/kernel.py
+++ mysql_kernel/kernel.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sqlalchemy as sa
 from ipykernel.kernelbase import Kernel
+import re
 
 
 __version__ = '0.4.1'
@@ -36,6 +37,51 @@ class MysqlKernel(Kernel):
                 'execution_count':self.execution_count,
                 'payload':[],
                 'user_expressions':{}}
+    
+    def generic_ddl(self, query, msg):
+        try:
+            with self.engine.connect() as con:
+                result = con.execute(sa.sql.text(query))
+                con.commit()
+                split_query = query.split()
+                if len(split_query) > 2:
+                    object_name = re.match("([^ ]+ ){2}(if (not )?exists )?([^ ]+)", query, re.IGNORECASE).group(4)
+                else:
+                    object_name = query.split()[1]
+                rows_affected = result.rowcount
+                if result.rowcount > 0:
+                    self.output((msg + '\nRows affected: %d.') % (object_name, rows_affected))
+                else:
+                    self.output(msg  % (object_name))
+                return
+        except Exception as msg:
+            self.output(str(msg))
+            return
+
+    def create_db(self, query):
+        self.generic_ddl(query, 'Database %s created successfully.')
+        
+
+    def drop_db(self, query):
+        self.generic_ddl(query, 'Database %s dropped successfully.')
+        
+    def create_table(self, query):
+        self.generic_ddl(query, 'Table %s created successfully.')
+        
+    def drop_table(self, query):
+        self.generic_ddl(query, 'Table %s dropped successfully.')
+
+    def delete(self, query):
+        self.generic_ddl(query, 'Data deleted from %s successfully.')
+    
+    def alter_table(self, query):
+        self.generic_ddl(query, 'Table %s altered successfully.')
+    
+    def insert_into(self, query):
+        self.generic_ddl(query, 'Data inserted into %s successfully.')
+    
+    def use_db(self, query):
+        self.generic_ddl(query, 'Changed to database %s successfully.')
 
     def do_execute(self, code, silent, store_history=True, user_expressions=None, allow_stdin=False):
         self.silent = silent
@@ -54,26 +100,28 @@ class MysqlKernel(Kernel):
                         else:
                             self.engine = sa.create_engine(f'mysql+py{v}')
                     elif l.startswith('create database '):
-                        pd.io.sql.execute(v, con=self.engine)
+                        self.create_db(v)
                     elif l.startswith('drop database '):
-                        pd.io.sql.execute(v, con=self.engine)
+                        self.drop_db(v)
                     elif l.startswith('create table '):
-                        pd.io.sql.execute(v, con=self.engine)
+                        self.create_table(v)
                     elif l.startswith('drop table '):
-                        pd.io.sql.execute(v, con=self.engine)
+                        self.drop_table(v)
                     elif l.startswith('delete '):
-                        pd.io.sql.execute(v, con=self.engine)
+                        self.delete(v)
                     elif l.startswith('alter table '):
-                        pd.io.sql.execute(v, con=self.engine)
+                        self.alter_table(v)
+                    elif l.startswith('use '):
+                        self.use_db(v)
                     elif l.startswith('insert into '):
-                        pd.io.sql.execute(v, con=self.engine)
+                        self.insert_into(v)
                     else:
                         if self.engine:
                             if ' like ' in l:
                                 if l[l.find(' like ')+6:].count('%')<4:
                                     self.output("sql code ' like %xx%' should be replace ' like %%xx%%'.")
                                     return self.ok()
-                            if l.startswith('select ') and ' limit ' not in l:
+                            if l.startswith('select ') and 'limit ' not in l:
                                 output = pd.read_sql(f'{v} limit 1000', self.engine).to_html()
                             else:
                                 output = pd.read_sql(v, self.engine).to_html()
